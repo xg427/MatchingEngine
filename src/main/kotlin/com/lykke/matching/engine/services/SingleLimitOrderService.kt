2ab0ccebeb7b19d30d3f5853c581dc3cceeca909
papchenko <rocketmannot@gmail.com>
2018-09-21 12:10:30 +0300

    LWDEV-9539 remove old limit order

--- src/main/kotlin/com/lykke/matching/engine/services/SingleLimitOrderService.kt
+++ src/main/kotlin/com/lykke/matching/engine/services/SingleLimitOrderService.kt
@@ -1,24 +1,22 @@
 package com.lykke.matching.engine.services
 
-import com.lykke.matching.engine.daos.v2.LimitOrderFeeInstruction
 import com.lykke.matching.engine.daos.LimitOrder
 import com.lykke.matching.engine.daos.fee.v2.NewLimitOrderFeeInstruction
 import com.lykke.matching.engine.daos.order.LimitOrderType
+import com.lykke.matching.engine.daos.v2.LimitOrderFeeInstruction
 import com.lykke.matching.engine.fee.listOfLimitOrderFee
 import com.lykke.matching.engine.messages.MessageStatus
-import com.lykke.matching.engine.messages.MessageType
 import com.lykke.matching.engine.messages.MessageWrapper
 import com.lykke.matching.engine.messages.ProtocolMessages
 import com.lykke.matching.engine.order.GenericLimitOrderProcessorFactory
 import com.lykke.matching.engine.order.OrderStatus
-import com.lykke.matching.engine.utils.PrintUtils
 import com.lykke.matching.engine.utils.NumberUtils
+import com.lykke.matching.engine.utils.PrintUtils
 import org.apache.log4j.Logger
 import java.math.BigDecimal
-import java.util.Date
-import java.util.UUID
+import java.util.*
 
-class SingleLimitOrderService(genericLimitOrderProcessorFactory: GenericLimitOrderProcessorFactory): AbstractService {
+class SingleLimitOrderService(genericLimitOrderProcessorFactory: GenericLimitOrderProcessorFactory) : AbstractService {
 
     companion object {
         private val LOGGER = Logger.getLogger(SingleLimitOrderService::class.java.name)
@@ -38,29 +36,11 @@ class SingleLimitOrderService(genericLimitOrderProcessorFactory: GenericLimitOrd
             parseMessage(messageWrapper)
         }
 
-        val order: LimitOrder
         val now = Date()
-        val isCancelOrders: Boolean
-
-        if (messageWrapper.type == MessageType.OLD_LIMIT_ORDER.type) {
-            val oldMessage = messageWrapper.parsedMessage!! as ProtocolMessages.OldLimitOrder
-            val uid = UUID.randomUUID().toString()
-            order = LimitOrder(uid, oldMessage.uid.toString(), oldMessage.assetPairId, oldMessage.clientId, BigDecimal.valueOf(oldMessage.volume),
-                    BigDecimal.valueOf(oldMessage.price), OrderStatus.InOrderBook.name, now, Date(oldMessage.timestamp), now, BigDecimal.valueOf(oldMessage.volume), null,
-                    type = LimitOrderType.LIMIT, lowerLimitPrice = null, lowerPrice = null, upperLimitPrice = null, upperPrice = null, previousExternalId = null)
-
-            LOGGER.info("Got old limit order messageId: ${messageWrapper.messageId} id: ${oldMessage.uid}, client ${oldMessage.clientId}, " +
-                    "assetPair: ${oldMessage.assetPairId}, " +
-                    "volume: ${NumberUtils.roundForPrint(oldMessage.volume)}, price: ${NumberUtils.roundForPrint(oldMessage.price)}, " +
-                    "cancel: ${oldMessage.cancelAllPreviousLimitOrders}")
-
-            isCancelOrders = oldMessage.cancelAllPreviousLimitOrders
-        } else {
-            val message = messageWrapper.parsedMessage!! as ProtocolMessages.LimitOrder
-            order = createOrder(message, now)
-            LOGGER.info("Got limit order ${incomingMessageInfo(messageWrapper.messageId, message, order)}")
-            isCancelOrders = message.cancelAllPreviousLimitOrders
-        }
+        val message = messageWrapper.parsedMessage!! as ProtocolMessages.LimitOrder
+        val order = createOrder(message, now)
+        LOGGER.info("Got limit order ${incomingMessageInfo(messageWrapper.messageId, message, order)}")
+        val isCancelOrders = message.cancelAllPreviousLimitOrders
 
         genericLimitOrderProcessor.processOrder(messageWrapper, order, isCancelOrders, now)
 
@@ -94,7 +74,7 @@ class SingleLimitOrderService(genericLimitOrderProcessorFactory: GenericLimitOrd
 
     private fun createOrder(message: ProtocolMessages.LimitOrder, now: Date): LimitOrder {
         val type = if (message.hasType()) LimitOrderType.getByExternalId(message.type) else LimitOrderType.LIMIT
-        val status = when(type) {
+        val status = when (type) {
             LimitOrderType.LIMIT -> OrderStatus.InOrderBook
             LimitOrderType.STOP_LIMIT -> OrderStatus.Pending
         }
@@ -126,33 +106,17 @@ class SingleLimitOrderService(genericLimitOrderProcessorFactory: GenericLimitOrd
         return ProtocolMessages.LimitOrder.parseFrom(array)
     }
 
-    private fun parseOldLimitOrder(array: ByteArray): ProtocolMessages.OldLimitOrder {
-        return ProtocolMessages.OldLimitOrder.parseFrom(array)
-    }
-
     override fun parseMessage(messageWrapper: MessageWrapper) {
-        if (messageWrapper.type == MessageType.OLD_LIMIT_ORDER.type) {
-            val message =  parseOldLimitOrder(messageWrapper.byteArray)
-            messageWrapper.messageId = if (message.hasMessageId()) message.messageId else message.uid.toString()
-            messageWrapper.timestamp = message.timestamp
-            messageWrapper.id = message.uid.toString()
-            messageWrapper.parsedMessage = message
-        } else {
-            val message =  parseLimitOrder(messageWrapper.byteArray)
-            messageWrapper.messageId = if (message.hasMessageId()) message.messageId else message.uid
-            messageWrapper.parsedMessage = message
-            messageWrapper.id = message.uid
-            messageWrapper.timestamp = message.timestamp
-        }
+        val message = parseLimitOrder(messageWrapper.byteArray)
+        messageWrapper.messageId = if (message.hasMessageId()) message.messageId else message.uid
+        messageWrapper.parsedMessage = message
+        messageWrapper.id = message.uid
+        messageWrapper.timestamp = message.timestamp
     }
 
     override fun writeResponse(messageWrapper: MessageWrapper, status: MessageStatus) {
-        if (messageWrapper.type == MessageType.OLD_LIMIT_ORDER.type) {
-            messageWrapper.writeResponse(ProtocolMessages.Response.newBuilder())
-        } else {
-            messageWrapper.writeNewResponse(ProtocolMessages.NewResponse.newBuilder()
-                    .setStatus(status.type))
-        }
+        messageWrapper.writeNewResponse(ProtocolMessages.NewResponse.newBuilder()
+                .setStatus(status.type))
     }
 }
 
